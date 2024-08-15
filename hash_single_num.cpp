f50fb4b60eb06676e073385faa4d87bde4c90cf9
akrashenin <77341643+akrashenin@users.noreply.github.com>
2024-08-15 13:04:22 +0300

    Add files via upload

---
+++ hash_single_num.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> nums{ 2,2,1 };
+//vector<int> nums{ 4, 1, 2, 1, 2 };
+//vector<int> nums{ 1 };
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        size_t uCount;
+        size_t uSize =nums.size();
+
+        for (size_t i = 0; i < uSize; i++)
+        {
+            uCount = 1;
+            for (size_t j = 0; j < uSize; j++)
+            {
+                if (nums[i] == nums[j] && i != j)
+                    uCount++;
+            }
+            if (uCount == 1)
+                return nums[i];
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    std::cout << "It's work!\n";
+    Solution sln;
+    int iRes = sln.singleNumber(nums);
+
+    cout << iRes;
+}
+
